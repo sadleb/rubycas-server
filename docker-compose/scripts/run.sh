63fe5b9945b5cacba901d552f53eeb0d31bfdc75
Kasun Geethaka <geethakacdsdt@gmail.com>
2019-05-26 22:27:32 +0530

    Update run.sh

--- docker-compose/scripts/run.sh
+++ docker-compose/scripts/run.sh
@@ -3,10 +3,6 @@
 #sleep 15s until db is up and running
 sleep 15
 
-eval "cat <<EOF
-$(</app/docker-compose/config/config.yml)
-EOF
-" | tee /app/docker-compose/config/appconfig.yml 2> /dev/null
-
+envsubst < /app/docker-compose/config/config.yml > /app/docker-compose/config/appconfig.yml
 
 bundle exec rubycas-server -p 3002 -c /app/docker-compose/config/appconfig.yml
