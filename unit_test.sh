fd74f13a601ccfdfe5f77460f9b5f878c515bafb
Kasun Geethaka <geethakacdsdt@gmail.com>
2019-07-15 23:23:15 +0530

    Update unit_test.sh

--- unit_test.sh
+++ unit_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# added a sleep since docker compose start can take time to start the application
 sleep 60 
 
 test_result=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3002/login)
